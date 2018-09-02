3dcfcf6941832fb324c31138bb0b33857a77a16e
nkalkstein <nkalkstein@gmail.com>
2018-09-02 18:29:56 -0400

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,30 @@
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <p>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={(event) => this.handleChange(event)}
+              placeholder="username" />
+          </p>
+          <p>
+            <input
+              type="text"
+              name="hometown"
+              value={this.state.hometown}
+              onChange={(event) => this.handleChange(event)}
+              placeholder="hometown" />
+          </p>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+
+
 export default function manageUsers(state, action){
 }
